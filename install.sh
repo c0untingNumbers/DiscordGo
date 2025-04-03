f50590dd91787885461e9aabd2d7f7fc0e10fa08
Lukas Peters <peterslukas0512@gmail.com>
2025-04-03 16:21:57 -0400

    removed unused code to make installation easier

--- install.sh
+++ install.sh
@@ -4,10 +4,5 @@
 # Installation script for discordC2.
 echo "Beginning DiscordGo C2 installation."
 
-printf "Please enter your server ID: "
-read server
-printf "Please enter your discord app token: "
-read token
-
 # Then run make
 make clean && make
